24694e352ad447fc05bc0ff569749fab757b365e
Sudan <sudan1bhandari@gmail.com>
2020-08-13 22:23:57 +0545

    changed

--- static/js/btnchange.js
+++ static/js/btnchange.js
@@ -50,4 +50,22 @@ function bluecolor(){
     }
 }
 
+
+
+function resetcolor(){
+    for(var i=0; i<allbutton.length;i++){
+        allbutton[i].classList.remove(allbutton[i].classList[1])
+        allbutton[i].classList.add(copybutton[i]);
+    }
+}
  
+
+function randomcolor(){
+    var choice=['btn-primary','btn-danger','btn-dark','btn-success','btn-link']
+    
+    for(var i=0; i<allbutton.length;i++){
+        var random=parseInt(Math.random()*5);
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add(choice[random])
+    }
+}
